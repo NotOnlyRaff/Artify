a5dcacd45dc417d141e884b4c5299eb3914210cf
NotOnlyRaff <72813891+NotOnlyRaff@users.noreply.github.com>
2026-04-23 19:47:38 +0200

    Update server URL in ServerConstant class

--- ArtifyFronted/lib/core/constants/server_constant.dart
+++ ArtifyFronted/lib/core/constants/server_constant.dart
@@ -1,6 +1,6 @@
 //class ServerConstant {
 //  static String serverURL =
-//  Platform.isAndroid ? 'https://artify-backend-1095834185305.europe-west2.run.app' : 'https://artify-backend-1095834185305.europe-west2.run.app';
+//  Platform.isAndroid ? 'https://artify-backend-1095834185305.europe-west2.run.app';
 //}
 
 class ServerConstant {
